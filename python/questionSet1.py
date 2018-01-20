38f8c5a69233cd0d48fac5c34b959852c62da630
Ardustorm <luke24601@gmail.com>
2018-01-20 13:48:46 -0800

    revised order of questionSet1

--- python/questionSet1.py
+++ python/questionSet1.py
@@ -1,25 +1,23 @@
 questions = [ ("Does morality exist?", ["Yes, it’s subjective", "Yes, it’s objective", "No"]),
-              ("Do you like candy?", [ "Love it!", "Kind of", "Sure?", "No"]),
               ("Is there meaning in life?", ["Yes", "No", "Doesn’t matter"]),
               ("Does God exist?", ["Yes", "No", "Unknowable", "Doesn’t matter"]),
               ("Do you believe you have a purpose?", ["Yes", "No", "Unsure", "Doesn’t matter"]),
+              ("Are you going to the Veritas Forum?", ["Of course", "No doubt", "Yes", "You betcha!"]), # Funny
               ("What is the inherent nature of humanity?", ["Good", "Evil", "Both good and evil", "Neither good nor evil"]),
-              ("Where did everything come from?", ["Random chance", "Uncreated Creator", "It's always been here", "Unknowable" ]),
+              ("Where did the universe come from?", ["Random chance", "Uncreated Creator", "It's always been here", "Unknowable" ]),
               ("Do you think you are a good person?", ["Yes", "No", "Unsure", "Doesn’t matter"]),
               ("Is there life after death?", ["Yes", "No", "Unknowable", "Doesn’t matter"]),
-              ("Are you going to the Veritas Forum?", ["Of course", "No doubt", "Yes", "You betcha!"]),
-              ("Are we in the Matrix?", ["Red pill", "Blue pill", "No, we are not in the Matrix", "No way to tell"]),
-              ("Does goodness exist?", ["Yes, it’s objective", "Yes, it’s subjective", "Doesn’t exist", "Doesn’t matter"]),
+              ("Are we in the Matrix?", ["Red pill", "Blue pill", "No, we are not in the Matrix", "No way to tell"]), # Funny
               ("Does truth exist?", ["Yes, it’s objective", "Yes, it’s subjective", "Doesn’t exist", "Doesn’t matter"]),
+              ("Does anything exist beyond the physical?", ["Yes", "No", "Unknowable", "Doesn’t matter"]),
               ("Where does your purpose come from?", ["Myself", "God", "Other people", "Nature"]),
-              ("What’s your default answer on scantrons?", ["", "", "", ""]),
               ("Are human beings special?", ["Yes", "No", "Doesn’t matter"]),
-              ("What do you strive for?", ["Happiness", "Success", "Peace", "Personal relationships"]),
+              ("What’s your default answer on scantrons?", ["", "", "", ""]), # Funny
+              ("What do you strive for?", ["Happiness", "Success", "Enlightenment", "Truth"]),
+              ("Does goodness exist?", ["Yes, it’s objective", "Yes, it’s subjective", "Doesn’t exist", "Doesn’t matter"]),
               ("Are you satisfied with your life?", ["Yes", "Kind of", "Not really", "Not at all"]),
-              ("Favorite place to eat on campus?", ["The Ave", "Metro", "VG's", "Mustang Station"]),
               ("Do you have a soul?", ["Yes", "No", "Not sure", "Unknowable"]),
               ("Can a world exist without evil?", ["Yes", "No", "Evil doesn’t exist", "Unknowable"]),
+              ("Favorite place to eat on campus?", ["The Ave", "Metro", "VG's", "Mustang Station"]), # Funny
               ("Does everything have an opposite?", ["Yes", "No", "Not sure"]),
-              ("Do anything exist beyond the physical?", ["Yes", "No", "Unknowable", "Doesn’t matter"]),
               ("Are the brain and the mind synonymous?", ["Yes", "No", "Unknowable", "Doesn’t matter"]) ]
-    
